3d7c3a29125f27f30ad8954dc06227491f8cbbe8
Leonardo Croda <leonardo.croda@euqueroinvestir.com>
2020-09-17 23:34:10 -0300

    remove imports in controllers index

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,14 +1,9 @@
-const users = require('./users');
-const groups = require('./groups');
-const expenses = require('./expenses');
-
 const index = (request, response) => {
   return response.send({ status: 'ok' });
 };
 
 module.exports = {
-  index,
-  users,
-  groups,
-  expenses,
+  async index(request, response) {
+    return response.send({ status: 'ok' });
+  },
 };
